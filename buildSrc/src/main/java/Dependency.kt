27b68f5c856b6e05bc18353928548da1191a0197
Pavel Indziukou <pahanez@gmail.com>
2019-03-14 22:12:36 +0300

    init buildSrc

---
+++ buildSrc/src/main/java/Dependency.kt
@@ -0,0 +1,11 @@
+object Versions {
+    val kotlin = "1.3.21"
+}
+
+object Dependencies {
+    val kotlinStandardLibrary = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:${Versions.kotlin}"
+}
+
+object Libraries {
+    val kotlin = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:${Versions.kotlin}"
+}
